ba7e7a223f9ec44f9c10d3c95f4c54f7395f7560
William Sercombe <wsercombe@gmail.com>
2021-07-05 11:58:59 -0700

    include readme and first test if user inputs 0

---
+++ README.md
@@ -0,0 +1,34 @@
+# _Homework-2_
+
+#### _This application quizzes the user to determine their preferred programing language_
+
+#### By _**William Sercombe**_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _GIT_
+* _Java Script_
+* _Visual Studio Text Editor_
+
+## Description
+
+*_The user answers multiple choice questions and the program has scores for each programming language, the language with the highest score is returned_
+
+## Setup/Installation Requirements
+
+* _The user should have access to the Github repository_
+* _The user should be able to clone the repository and view the application on a web browser_
+
+## Known Bugs
+
+* _I have no logic for when there is a tie_
+
+## License
+
+_N/A_
+
+## Contact Information
+
+_William Sercombe | wsercombe@gmail.com | 503-545-9535_
--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
+function processNumber(userNumber){
+  let outputArray = [];
+  let userNumberString;
+  for (let i = 0; i <= userNumber; i++) {
+    userNumberString = i.toString();
+    /*
+    if (userNumberString.includes("3")){
+      outputArray.push("Won't you be my neighbor?");
+    } else if (userNumberString.includes("2")) {
+      outputArray.push("Boop");
+    } else if (userNumberString.includes("1")) {
+      outputArray.push("Beep");
+    } else {
+      outputArrapy.push(i);
+    }
+    */
+   outputArray.push(userNumberString);
+  }
+  return outputArray;
+}
+
+
+
+
 $(document).ready(function(event) {
   $("#number-beeper").submit(function(event) {
     const userNumber = parseInt($("#user-number").val());
-    $("#result-section").show();
-    $("#user-output").text(userNumber);
+    const outputArray = processNumber(userNumber);
 
+    $("#result-section").show();
+    $("#user-output").text(outputArray.join(", "));
+  
     event.preventDefault();
   });
 });
